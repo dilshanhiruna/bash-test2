01a6a99553151464650ad0001ae29f443e21b77b
dilshanhiruna <dilshan.hirunaa@gmail.com>
2022-06-28 15:33:12 +0530

    c

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
-echo "start 125"
+echo "start 126"
 
 #import text from https://raw.githubusercontent.com/dilshanhiruna/bash-test2/main/code-templates/test/hi.txt and save it to a variable IFS
 text=($(curl -s https://raw.githubusercontent.com/dilshanhiruna/bash-test2/main/code-templates/test/hi.txt))
 
 echo "Hi from test.sh"
 
-echo $text
+cat $text
