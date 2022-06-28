d9b3687a1411a8dd5f1606d33051fd4f65179252
dilshanhiruna <dilshan.hirunaa@gmail.com>
2022-06-28 12:11:54 +0530

    c

--- test.sh
+++ test.sh
@@ -2,6 +2,6 @@ echo "start"
 
 FILE=$(-s <curl https://raw.githubusercontent.com/dilshanhiruna/bash-test2/main/code-templates/test/hi.txt)
 
-echo "Hi from test.sh"
+echo "Hi from test.sh 111"
 
 echo "file: $FILE"
