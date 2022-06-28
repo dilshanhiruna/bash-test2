09acf9d342d4ff84784750004e506a890b9ee4bc
dilshanhiruna <dilshan.hirunaa@gmail.com>
2022-06-28 16:01:56 +0530

    c

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 echo "start 127"
 
 #import text from https://raw.githubusercontent.com/dilshanhiruna/bash-test2/main/code-templates/test/hi.txt and save it to a variable IFS
-IFS=$'\n'
 text=($(curl -s https://raw.githubusercontent.com/dilshanhiruna/bash-test2/main/code-templates/test/hi.txt))
 
-echo "Hi from test.sh"
+#write text to a file
+echo "${text}" >test.txt
 
-cat $text
+echo "Hi from test.sh"
