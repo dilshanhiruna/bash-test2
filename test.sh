b8b22c8155391cddad56fc33b053ffa2f8d53881
dilshanhiruna <dilshan.hirunaa@gmail.com>
2022-06-28 12:03:40 +0530

    c

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
-# FILE=$(<code-templates/test/hi.txt)
+FILE=$(<https://raw.githubusercontent.com/dilshanhiruna/bash-test2/main/code-templates/test/hi.txt)
 
 echo "Hi from test.sh"
 
-# echo "$FILE"
+echo "$FILE"
