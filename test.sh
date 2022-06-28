c9b66de98c6945a7dc84208e7285b03a59b1742b
dilshanhiruna <dilshan.hirunaa@gmail.com>
2022-06-28 16:10:48 +0530

    c

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
-echo "start 128"
+echo "start 129"
 
 #import text from https://raw.githubusercontent.com/dilshanhiruna/bash-test2/main/code-templates/test/hi.txt and save it to FILE
 
-echo curl -s https://raw.githubusercontent.com/dilshanhiruna/bash-test2/main/code-templates/test/hi.txt >$FILE
+echo https://raw.githubusercontent.com/dilshanhiruna/bash-test2/main/code-templates/test/hi.txt >$FILE
 
 echo "Hi from test.sh"
