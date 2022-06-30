08650914eb68eed25ce57d26bd0cac1f6805fe26
dilshanhiruna <dilshan.hirunaa@gmail.com>
2022-06-30 10:16:34 +0530

    c

--- test.sh
+++ test.sh
@@ -1,5 +1,32 @@
-echo "start 132"
+echo "start 1355"
 
-echo "$(curl -s https://raw.githubusercontent.com/dilshanhiruna/bash-test2/main/code-templates/test/hi.txt)" >index.txt
+get_file_path() {
 
-echo "Hi from test.sh"
+    # $1 = enviroment
+    # $2 = file path (eg: code-templates/test/hi.txt)
+    # $3 = host (eg: https://raw.githubusercontent.com/rootaid/bash/main)
+
+    if [ $1 == "development" ]; then
+        URL= $2
+        return <$URL
+    fi
+    if [ $1 == "production" ]; then
+        URL="curl -s $3/$2"
+        return $URL
+    fi
+
+}
+
+# URL="curl -s https://raw.githubusercontent.com/dilshanhiruna/bash-test2/main/code-templates/test/hi.txt"
+
+# SOURCE="$($URL)"
+
+# echo "$SOURCE" >index.ts
+
+# ##############################################################################
+
+# URL="code-templates/test/hi.txt"
+
+SOURCE="$($get_file_path production code-templates/test/hi.txt https://raw.githubusercontent.com/rootaid/bash/main)"
+
+echo "$SOURCE" >index.ts
